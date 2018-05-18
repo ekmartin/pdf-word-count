e8608e71e253a4536b34659e76579d4aec14cc44
Martin Ek <mail@ekmartin.com>
2018-05-18 20:55:42 +0200

    Support reading from a file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
+extern crate clap;
 extern crate pdf_word_count;
 
-use std::io;
+use clap::{App, Arg};
 use pdf_word_count::Collector;
+use std::fs::File;
+use std::io;
 
 fn main() {
-    println!("{}", Collector::process_document(io::stdin()))
+    let args = App::new("pdf-wc")
+        .version("0.1.0")
+        .about("Displays the number of words, lines and characters in a PDF.")
+        .arg(
+            Arg::with_name("file")
+                .help("File to read from")
+                .required(false),
+        )
+        .get_matches();
+
+    let word_count = match args.value_of("file") {
+        Some(filename) => {
+            let file = File::open(filename).expect("Failed opening file");
+            Collector::process_document(file)
+        }
+        None => Collector::process_document(io::stdin()),
+    };
+
+    println!("{}", word_count);
 }
